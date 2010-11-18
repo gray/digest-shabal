62ef1b870292db31dd4ab0834b530d61710871fc
gray <gray@cpan.org>
2010-11-17 23:43:08 -0800

    add performance section to the pod

--- lib/Digest/Shabal.pm
+++ lib/Digest/Shabal.pm
@@ -153,6 +153,76 @@ The constructor requires the algorithm to be specified. It must be one of:
 
 Returns the algorithm used by the object.
 
+=head1 PERFORMANCE
+
+This distribution contains a benchmarking script which compares the various
+message digest algorithms available on CPAN. These are the results on
+a MacBook 2GHz Core 2 Duo (64-bit) with Perl 5.12.2, using a message size of
+1KB:
+
+md5          245759/s  240 MB/s
+skein_512    227104/s  222 MB/s
+bmw_384      215039/s  210 MB/s
+bmw_512      214369/s  209 MB/s
+skein_256    194606/s  190 MB/s
+blake_384    158510/s  155 MB/s
+blake_512    150312/s  147 MB/s
+bmw_224      131523/s  128 MB/s
+bmw_256      131522/s  128 MB/s
+blake_224    119301/s  117 MB/s
+skein_1024   119300/s  117 MB/s
+blake_256    119300/s  117 MB/s
+sha1         115924/s  113 MB/s
+shabal_224    96376/s   94 MB/s
+shabal_512    95467/s   93 MB/s
+shabal_256    95467/s   93 MB/s
+shabal_384    94575/s   92 MB/s
+sha_256       75918/s   74 MB/s
+sha_512       75918/s   74 MB/s
+sha_384       73080/s   71 MB/s
+sha_224       73080/s   71 MB/s
+keccak_224    60151/s   59 MB/s
+keccak_256    60151/s   59 MB/s
+luffa_224     54613/s   53 MB/s
+luffa_256     54613/s   53 MB/s
+keccak_384    49321/s   48 MB/s
+shavite3_256  47287/s   46 MB/s
+shavite3_224  47287/s   46 MB/s
+fugue_256     46849/s   46 MB/s
+fugue_224     46849/s   46 MB/s
+md6_224       46419/s   45 MB/s
+md6_256       43952/s   43 MB/s
+luffa_384     40193/s   39 MB/s
+echo_256      39097/s   38 MB/s
+echo_224      38745/s   38 MB/s
+md6_384       35544/s   35 MB/s
+keccak_512    34133/s   33 MB/s
+fugue_384     31508/s   31 MB/s
+simd_256      30632/s   30 MB/s
+simd_224      30351/s   30 MB/s
+md6_512       30075/s   29 MB/s
+shavite3_512  29805/s   29 MB/s
+shavite3_384  29805/s   29 MB/s
+luffa_512     29538/s   29 MB/s
+cubehash_384  28183/s   28 MB/s
+cubehash_256  28183/s   28 MB/s
+cubehash_512  28109/s   27 MB/s
+cubehash_224  28109/s   27 MB/s
+hamsi_256     24889/s   24 MB/s
+hamsi_224     24661/s   24 MB/s
+whirlpool     24661/s   24 MB/s
+fugue_512     23209/s   23 MB/s
+echo_512      21154/s   21 MB/s
+echo_384      20958/s   20 MB/s
+simd_512      17454/s   17 MB/s
+simd_384      17454/s   17 MB/s
+jh_384        14354/s   14 MB/s
+jh_512        14221/s   14 MB/s
+jh_224        14221/s   14 MB/s
+jh_256        14221/s   14 MB/s
+hamsi_384      7587/s    7 MB/s
+hamsi_512      7587/s    7 MB/s
+
 =head1 SEE ALSO
 
 L<Digest>
